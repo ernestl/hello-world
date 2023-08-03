86ff1167f58239f2252b9d334166b5a4e8c8a4d4
ernestl <ernest.lotter@canonical.com>
2023-08-03 20:48:50 +0200

    stuff

--- hello-world.go
+++ hello-world.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	
+	fmt.Printf("Real UID: %d\n", os.Getuid())
+	fmt.Printf("Real GID: %d\n", os.Getgid())
+
+	userInfo, err := user.LookupId(fmt.Sprintf("%d", os.Getuid()))
+	fmt.Printf("Real user home: %s\n", userInfo.HomeDir)
+	fmt.Printf("Error: %v\n", err)
+
+	fmt.Printf("Effec UID: %d\n", os.Geteuid())
+        fmt.Printf("Effec GID: %d\n", os.Getegid())
+
+	userInfo, err = user.LookupId(fmt.Sprintf("%d", os.Geteuid()))
+	fmt.Printf("Effec user home: %s\n", userInfo.HomeDir)
+	fmt.Printf("Error: %v\n", err)
+
+	fmt.Println("Hello World!")
 }
